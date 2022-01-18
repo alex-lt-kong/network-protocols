3ab8b49f5f824f5a10f0d14036c4be5041055c56
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-01-18 23:12:13 +0800

    remove obsolete code

--- cgi/10_http-authentication.cpp
+++ cgi/10_http-authentication.cpp
@@ -1,44 +1,8 @@
 #include<iostream>
-#include<chrono>
 #include<cstring>
-#include <ctime>
 #include <fstream>
-#include "json/json.h"
 
 using namespace std;
-
-vector<string> split(string str, string token){
-    vector<string>result;
-    while(str.size()){
-        int index = str.find(token);
-        if(index!=string::npos){
-            result.push_back(str.substr(0,index));
-            str = str.substr(index+token.size());
-            if(str.size()==0)result.push_back(str);
-        }else{
-            result.push_back(str);
-            str = "";
-        }
-    }
-    return result;
-}
-
-int handleGET404Status() {
-
-    cout << "Content-Type: text/plain;\n"
-         << "Status: 404\n" << endl;
-    cout << "Not found" << endl;
-    return 0; 
-}
-
-
-int handleGET202Status() {
-
-    cout << "Content-Type: text/plain;\n"
-         << "Status: 202 Accepted\n" << endl;
-    cout << "Good" << endl;
-    return 0; 
-}
 
 int main(){
 
