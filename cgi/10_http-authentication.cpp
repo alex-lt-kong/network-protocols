08442aa2ca97313935d3f49a80f0ea068a194181
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-01-19 23:11:58 +0800

    more complete implementation of HTTP basic authentication

--- cgi/10_http-authentication.cpp
+++ cgi/10_http-authentication.cpp
@@ -1,8 +1,54 @@
 #include<iostream>
 #include<cstring>
-#include <fstream>
+#include<fstream>
+#include<vector>
+#include <algorithm>
 
 using namespace std;
+
+vector<string> split(string str, string token){
+    vector<string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index!=string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
+
+string base64_decode(const std::string &in) {
+
+    string out;
+
+    vector<int> T(256,-1);
+    for (int i=0; i<64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i;
+
+    int val=0, valb=-8;
+    for (unsigned char c : in) {
+        if (T[c] == -1) break;
+        val = (val << 6) + T[c];
+        valb += 6;
+        if (valb >= 0) {
+            out.push_back(char((val>>valb)&0xFF));
+            valb -= 8;
+        }
+    }
+    return out;
+}
+
+int unauthorized(string debugInfo) {
+    cout << "Content-Type: text/plain\n"
+        << "WWW-Authenticate: Basic realm=\"Hi! HTTP Authentication is enabled for this page!\"\n"
+        << "Info: " << debugInfo << "\n"
+        << "Status: 401 Unauthorized\n" << endl;
+    return 0; 
+}
 
 int main(){
 
@@ -24,13 +70,22 @@ int main(){
     if (getenv("HTTP_AUTHORIZATION") == NULL) {
         // Environment variable HTTP_AUTHORIZATION is only available after
         // you set CGIPassAuth to On in Apache config file.
-        cout << "Content-Type: text/plain\n"
-             << "WWW-Authenticate: Basic realm=\"Hi! HTTP Authentication is enabled for this page!\"\n"
-             << "Status: 401 Unauthorized\n" << endl;
-        return 0; 
+        return unauthorized("getenv(\"HTTP_AUTHORIZATION\") == NULL");
     }
+    string auth = getenv("HTTP_AUTHORIZATION");
+    transform(auth.begin(), auth.end(), auth.begin(), ::tolower); // just .lower(), C++'s way...
+    if (auth.rfind("basic ", 0) != 0) {  // just .startsWith(), C++'s way...
+        return unauthorized("auth.rfind(\"basic \", 0) != 0");
+    }
+    auth = getenv("HTTP_AUTHORIZATION");
+    auth.erase(0, 6);
+    vector<string> cred = split(base64_decode(auth), ":");
+    if (cred.size() != 2) {
+        return unauthorized("cred.size() != 2");
+    }
+
     cout << "Content-Type: text/plain\n" << endl;
     cout << "You are in:\n";
-    cout << "HTTP_AUTHORIZATION:" << getenv("HTTP_AUTHORIZATION") << endl;
+    cout << "Username:" << cred[0] << "\nPassword:" << cred[1] << endl;
     return 0;
 }
