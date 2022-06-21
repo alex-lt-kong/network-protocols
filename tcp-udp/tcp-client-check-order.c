c2e5852d6b822e38fbc5f40bc4e8610b6b3b711b
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-06-21 16:25:08 +0800

    TCP check data order

--- tcp-udp/tcp-client-check-order.c
+++ tcp-udp/tcp-client-check-order.c
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
     int sock = 0;
     
     struct sockaddr_in serv_addr;
-    char msg[64];// = "msg from client at %ld";
+    
     char buffer[READ_BUF_SIZE] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -31,7 +31,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT);
     
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+    if(inet_pton(AF_INET, "0.0.0.0", &serv_addr.sin_addr)<=0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
@@ -40,10 +40,19 @@ int main(int argc, char const *argv[])
         perror("In connect()");
         return -1;
     }
+
     long bytes = -1;
-    for (int i = 0; i < 10000; ++i) {
+    char msg[MSG_BUF_SIZE];// = "msg from client at %ld";
+    int msg_len = -1; 
+    int msg_padding_lens[] = {0, 1024, 32767, 60000};
+    for (int i = 0; i < 100; ++i) {
         gettimeofday(&tp, NULL);
-        snprintf(msg, MSG_BUF_SIZE, "%d.%06d: msg from client\n", tp.tv_sec, tp.tv_usec);
+        memset(msg, 0, sizeof(msg));
+        snprintf(msg, MSG_BUF_SIZE, "%d.%06d: msg from client", tp.tv_sec, tp.tv_usec);
+        msg_len = strlen("1655714250.706089: msg from client");
+        memset(msg + msg_len, 'a', msg_padding_lens[rand() % sizeof(msg_padding_lens) / sizeof(int)]);
+        msg_len = strlen(msg);
+        msg[msg_len] = '\n';
         if (send(sock , msg , strlen(msg), 0) != -1) {
             // use send() instead of write() and use MSG_MORE to send messages in batches.
             // This, however, doesn't make much difference since we can accumulate the message and them
