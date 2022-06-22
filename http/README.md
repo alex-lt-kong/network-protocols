52faa4a1e1fa336dd377623fa49a5bc4716d811c
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-06-22 14:31:30 +0800

    a bit more on HTTP file

---
+++ http/README.md
@@ -0,0 +1,6 @@
+# Notes
+
+* HTTP is a complex protocol, [RFC2616 -- Hypertext Transfer Protocol -- HTTP/1.1](https://datatracker.ietf.org/doc/html/rfc2616) is 175 pages long.
+
+* Server-side programs in this directory could be inefficient, insecure or unstable--they are just a few simple
+  experiments roughly demonstrating how HTTP works.
--- http/server-file.c
+++ http/server-file.c
@@ -21,6 +21,37 @@ char* get_iso_datetime(char* buf) {
     return buf;
 }
 
+/**
+ * @brief Get the file object
+ * users are reminded to free() the buffer pointer themselves.
+ * @param filename 
+ * @return pointer to bytes of the specified file or NULL if error occurs.
+ */
+unsigned char* get_file(char* file_name, size_t* file_len) {
+    char *file_buffer;
+    FILE *file;
+    //Open file
+    file = fopen(file_name, "rb");
+    if (!file) {
+        perror("In fopen()");
+        return NULL;
+    }
+
+    
+    fseek(file, 0, SEEK_END);
+    *file_len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    file_buffer=(unsigned char*)malloc(*file_len + 1);
+    if (!file_buffer) {
+        perror("In malloc()");        
+        fclose(file);
+        return NULL;
+    }
+    fread(file_buffer, *file_len, 1, file);
+    fclose(file);
+    return file_buffer;
+}
+
 int main() {
     int server_fd;
     char iso_dt[iso_dt_len];
@@ -33,78 +64,22 @@ int main() {
     int addrlen = sizeof(address);
     char header[HEADER_BUF_SIZE] = {0};
     char* resp = NULL;
-
-    FILE *file;
-    char *file_buffer;
-    size_t file_len = 0, resp_len = 0;
-
-    //Open file
-    file = fopen("server-file.mp4", "rb");
-    if (!file) {
-        perror("In fopen()");
-        return EXIT_FAILURE;
-    }
-
-    //Get file length
-    fseek(file, 0, SEEK_END);
-    file_len = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    
-    file_buffer=(unsigned char*)malloc(file_len+1);
-    if (!file_buffer) {
-        perror("In malloc()");        
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-    
-    snprintf(header, HEADER_BUF_SIZE,
-        "HTTP/1.1 200 OK\n"     
-        "Server: Snakeoil/2.2.3\n"
-        "Content-Type: video/mp4\n"
-        "Content-Length: %d\n"
-        "Accept-Ranges: bytes\n"
-        "Connection: close\n"
-        "\n",
-    file_len);
-     //Read file contents into buffer
-    fread(file_buffer, file_len, 1, file);
-    fclose(file);
-
-    
-    resp = (unsigned char*)malloc(strlen(header) + file_len);
-    strcpy(resp, header);
-    memcpy(resp + strlen(header), file_buffer, file_len);
-    resp_len = strlen(header) + file_len;
-    free(file_buffer);
+    unsigned char* file_buffer;
+    size_t resp_len = -1, file_len = -1;
     
     
-    /* htonl converts a long integer (e.g. address) to a network representation */
     memset((char*)&address, 0, sizeof(address)); 
-    address.sin_family = AF_INET;
-    // sin_family is always set to AF_INET.  This is required. sin stands for sockaddr_in.
-    address.sin_addr.s_addr = htonl(INADDR_ANY); 
-    // It does NOT "generate a random IP". It binds the socket to all available interfaces, i.e., binds to 0.0.0.0.
+    address.sin_family = AF_INET;    
+    address.sin_addr.s_addr = htonl(INADDR_ANY);     
     address.sin_port = htons(HTTP_PORT);
-    // port in network byte order. htons() translates a short integer from host byte order to network byte order
+    
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { 
-        /* To bind/name a socket, means assigning a transport address to the socket. In the case of IP networking, it
-         * is a port number. In sockets, this operation is called binding an address.
-         */
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("In bind()"); 
         return 0; 
     }
 
     if (listen(server_fd, 10) < 0) {
-        /* listen() marks the socket referred to by sockfd as a passive socket, that is, as a socket that will be
-         * used to accept incoming connection requests using accept().
-         * sockfd: a file descriptor that refers to a socket of type SOCK_STREAM or SOCK_SEQPACKET.
-         * backlog, defines the maximum number of pending connections that can be queued up before connections are refused.
-         *
-         * When listen(2) is called on an unbound socket, the socket is automatically bound to a random free port with the
-         * local address set to INADDR_ANY.
-        */
         perror("In listen()");
         exit(EXIT_FAILURE);
     }
@@ -112,7 +87,7 @@ int main() {
     int new_socket;
     while(1) {
         new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-        printf("New connection accept()'ed");
+        printf("[%s] New connection accept()'ed\n", get_iso_datetime(iso_dt));
         if (new_socket < 0) {            
             perror("In accept()");
             free(resp);
@@ -120,18 +95,37 @@ int main() {
         }
         
         char buffer[READ_BUF_SIZE] = {0};
-        if (read(new_socket, buffer, READ_BUF_SIZE) != -1) {
-            printf("[%s] Received: [\n%s]\n", get_iso_datetime(iso_dt), buffer);
+        size_t bytes_read = -1;
+        if (bytes_read = read(new_socket, buffer, READ_BUF_SIZE) != -1) {
+            printf("========== Data received at [%s]: ==========\n%s\n---------- Data ends ----------\n", get_iso_datetime(iso_dt), buffer);
+            file_buffer = get_file("server-file.mp4", &file_len);
+    
+            snprintf(header, HEADER_BUF_SIZE,
+                "HTTP/1.1 200 OK\n"     
+                "Server: Snakeoil/2.2.3\n"
+                "Content-Type: video/mp4\n"
+                "Content-Length: %d\n"
+                "Accept-Ranges: bytes\n"
+                "Connection: close\n"
+                // Connection: closeIndicates that either the client or the server would like to close the connection.
+                // This is the default on HTTP/1.0 requests. (On HTTP/1.1 requests, the default value is keep-alive.)
+                "\n",
+            file_len);
+            resp = (unsigned char*)malloc(strlen(header) + file_len);
+            strcpy(resp, header);
+            memcpy(resp + strlen(header), file_buffer, file_len);
+            resp_len = strlen(header) + file_len;
+            free(file_buffer);
             if (write(new_socket, resp ,resp_len) != -1) {
-                printf("Response sent");
+                printf("Response sent\n");
             } else {
                 perror("In write()");
             }
         } else {
             perror("In read()");
-        }
-        
+        }        
         close(new_socket);
+        printf("[%s] Connection close()'ed gracefully\n\n", get_iso_datetime(iso_dt));
     }
     close(server_fd);
     return 0;
