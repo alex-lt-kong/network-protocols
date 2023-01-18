a07e8462691706258dae0f3ca62ff0c58d2241c7
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-01-18 21:18:55 +0800

    update

--- message-queues/zmq-server-multiple.cpp
+++ message-queues/zmq-server-multiple.cpp
@@ -16,43 +16,47 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  const string endpoint = "tcp://*:4242";
+    srand(time(NULL));
+    const string endpoint = "tcp://*:4242";
 
-  // initialize the 0MQ context
-  zmqpp::context context;
+    // initialize the 0MQ context
+    zmqpp::context context;
 
-  // generate a pull socket
-  zmqpp::socket_type type = zmqpp::socket_type::pull;
-  zmqpp::socket socket (context, type);
+    // generate a pull socket
+    zmqpp::socket_type type = zmqpp::socket_type::pull;
+    zmqpp::socket socket (context, type);
 
-  const size_t batch_size = 20;
-  assert (TEST_SIZE % batch_size == 0);
-  const size_t batch_count = TEST_SIZE / batch_size;
-  
-  zmqpp::message_t message;
-  vector<vector<person_struct>> persons{batch_count};
-  vector<string> messages{batch_count};
-  for (size_t i = 0; i < batch_count; ++i) {    
-    messages[i].reserve(256);
-  }
-  cout << "Binding to " << endpoint << "..." << endl;
-  socket.bind(endpoint);
-  for (size_t i = 0; i < batch_count; ++i) {
-    socket.receive(message);
-    messages[i] = message.get(0);
-  }
+    const size_t batch_size = 20;
+    assert (TEST_SIZE % batch_size == 0);
+    const size_t batch_count = TEST_SIZE / batch_size;
+    
+    zmqpp::message_t message;
+    vector<string> messages{batch_count};
+    for (size_t i = 0; i < batch_count; ++i) {    
+        messages[i].reserve(256);
+    }
+    cout << "Binding to " << endpoint << "..." << endl;
+    socket.bind(endpoint);
+    for (size_t i = 0; i < batch_count; ++i) {
+        socket.receive(message);
+        messages[i] = message.get(0);
+    }
 
-  for (size_t i = 0; i < batch_count; ++i) {
-    auto received_array = kj::ArrayPtr<capnp::word>(
-      reinterpret_cast<capnp::word*>((char*)(messages[i].data())),
-      messages[i].size() * sizeof(capnp::word)
-    );
-    persons[i] = vector<person_struct>{batch_size};
-    decodeBytesToStructsCapnp(received_array, persons[i]);
-  }
+    vector<person_struct> persons{batch_size};
+    vector<person_struct> persons_sample{batch_size};
+    size_t sample_idx = rand() % batch_count;
+    for (size_t i = 0; i < batch_count; ++i) {
+        auto received_array = kj::ArrayPtr<capnp::word>(
+            reinterpret_cast<capnp::word*>((char*)(messages[i].data())),
+            messages[i].size() * sizeof(capnp::word)
+        );
+        decodeBytesToStructsCapnp(received_array, persons);
+        if (sample_idx == i) {
+            persons_sample = persons;
+        }
+    }
 
 
-  cout << getStringRepresentationOfPerson(
-    persons[rand() % persons.size()][rand() % batch_size]) << endl;
+    cout << getStringRepresentationOfPerson(persons_sample[rand() % batch_size]) << endl;
 }
 
--- message-queues/zmq-server-single.cpp
+++ message-queues/zmq-server-single.cpp
@@ -16,39 +16,44 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  const string endpoint = "tcp://*:4242";
+    const string endpoint = "tcp://*:4242";
 
-  // initialize the 0MQ context
-  zmqpp::context context;
+    // initialize the 0MQ context
+    zmqpp::context context;
 
-  // generate a pull socket
-  zmqpp::socket_type type = zmqpp::socket_type::pull;
-  zmqpp::socket socket (context, type);
+    // generate a pull socket
+    zmqpp::socket_type type = zmqpp::socket_type::pull;
+    zmqpp::socket socket (context, type);
 
-  // bind to the socket
-  
-  zmqpp::message_t message;
-  vector<person_struct> persons{TEST_SIZE};
-  vector<string> messages{TEST_SIZE};
-  for (size_t i = 0; i < TEST_SIZE; ++i) {    
-    messages[i].reserve(256);
-  }
-  cout << "Binding to " << endpoint << "..." << endl;
-  socket.bind(endpoint);
-  for (size_t i = 0; i < TEST_SIZE; ++i) {
-    socket.receive(message);
-    messages[i] = message.get(0);
-  }
+    // bind to the socket
+    
+    zmqpp::message_t message;
+    vector<string> messages{TEST_SIZE};
+    for (size_t i = 0; i < TEST_SIZE; ++i) {    
+        messages[i].reserve(256);
+    }
+    cout << "Binding to " << endpoint << "..." << endl;
+    socket.bind(endpoint);
+    for (size_t i = 0; i < TEST_SIZE; ++i) {
+        socket.receive(message);
+        messages[i] = message.get(0);
+    }
 
-  for (size_t i = 0; i < TEST_SIZE; ++i) {
-    auto received_array = kj::ArrayPtr<capnp::word>(
-      reinterpret_cast<capnp::word*>((char*)(messages[i].data())),
-      messages[i].size() * sizeof(capnp::word)
-    );
-    decodeBytesToStructCapnp(received_array, persons[i]);
-  }
+    person_struct person;
+    person_struct person_sample;
+    size_t sample_idx = rand() % TEST_SIZE;
+    for (size_t i = 0; i < TEST_SIZE; ++i) {
+        auto received_array = kj::ArrayPtr<capnp::word>(
+            reinterpret_cast<capnp::word*>((char*)(messages[i].data())),
+            messages[i].size() * sizeof(capnp::word)
+        );
+        decodeBytesToStructCapnp(received_array, person);
+        if (sample_idx == i) {
+            person_sample = person;
+        }
+    }
 
 
-  cout << getStringRepresentationOfPerson(persons[rand() % persons.size()]) << endl;
+    cout << getStringRepresentationOfPerson(person) << endl;
 }
 
